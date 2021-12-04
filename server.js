f319d9c1c55bc83150606c3cb60ff48d322bc58b
Manraj <manraj.pannu@ryerson.ca>
2021-12-04 01:28:30 -0500

    22

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const { request, response } = require('express');
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const axios = require('axios');
-
+app.use(express.static(path.join(__dirname, 'my-app/build')));
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
